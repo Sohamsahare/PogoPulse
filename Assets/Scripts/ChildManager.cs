39a6d2ec46116039d47475434d571d547f506f5a
Soham Sahare <sohamsahare@gmail.com>
2018-09-13 21:32:54 +0530

    First commit

---
+++ Assets/Scripts/ChildManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ChildManager : MonoBehaviour
+{
+    public GameObject childObject;
+    public Color color;
+
+    private float width;
+    private SpriteRenderer spriteRenderer;
+    private Vector3 spawnPosition;
+
+    private void Awake()
+    {
+        spriteRenderer = childObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        width = spriteRenderer.bounds.size.x;
+        spriteRenderer.color = color;
+        spawnPosition = transform.position;
+        SpawnChild(spawnPosition);
+    }
+
+    public void SpawnChild(Vector3 spawnPosition)
+    {
+        Instantiate(childObject, spawnPosition, Quaternion.identity, transform);
+    }
+
+}
