7c82a588dab4311be4bcdcc0091b911f1859c677
Soham Sahare <sohamsahare@gmail.com>
2018-09-15 18:14:53 +0530

    added initial parallax code and self spawning backgrounds

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+
+    public Transform target;
+    public float moveAfterThisOffset = 9f;
+    private void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.position.x + moveAfterThisOffset > transform.position.x)
+        {
+            transform.position += new Vector3((target.position.x + moveAfterThisOffset), transform.position.y, transform.position.z);
+        }
+        else if (target.position.x - moveAfterThisOffset < transform.position.x)
+        {
+            transform.position -= new Vector3((target.position.x - moveAfterThisOffset), transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    public float backgroundSize;
+    public float parallaxSpeed;
+    public bool isScrolling;
+    public bool isParallaxing;
+
+    [SerializeField]
+    private float viewZone = 10;
+    private Transform camTransform;
+    private Transform[] layers;
+    private int leftIndex;
+    private int rightIndex;
+    private float lastCameraX;
+
+    private void Awake()
+    {
+        camTransform = Camera.main.transform;
+        lastCameraX = camTransform.position.x;
+        layers = new Transform[transform.childCount];
+        for (int i = 0; i < layers.Length; i++)
+        {
+            layers[i] = transform.GetChild(i);
+        }
+        leftIndex = 0;
+        rightIndex = layers.Length - 1;
+    }
+
+    private void ScrollLeft()
+    {
+        int lastRight = rightIndex;
+        layers[rightIndex].position = new Vector3((layers[leftIndex].position.x - backgroundSize), layers[rightIndex].position.y, layers[rightIndex].position.z);
+        leftIndex = rightIndex;
+        rightIndex--;
+        if (rightIndex < 0)
+        {
+            rightIndex = layers.Length - 1;
+        }
+    }
+
+    private void ScrollRight()
+    {
+        int lastLeft = leftIndex;
+        layers[leftIndex].position = new Vector3((layers[rightIndex].position.x + backgroundSize), layers[leftIndex].position.y, layers[leftIndex].position.z);
+        rightIndex = leftIndex;
+        leftIndex++;
+        if (leftIndex == layers.Length)
+        {
+            leftIndex = 0;
+        }
+    }
+
+    private void Update()
+    {
+        if (isParallaxing)
+        {
+            float deltaX = camTransform.position.x - lastCameraX;
+            transform.position += Vector3.right * deltaX * parallaxSpeed;
+        }
+
+        lastCameraX = camTransform.position.x;
+
+        if (isScrolling)
+        {
+            if (camTransform.position.x < layers[leftIndex].position.x + viewZone)
+            {
+                ScrollLeft();
+            }
+            if (camTransform.position.x > layers[rightIndex].position.x - viewZone)
+            {
+                ScrollRight();
+            }
+        }
+    }
+}
